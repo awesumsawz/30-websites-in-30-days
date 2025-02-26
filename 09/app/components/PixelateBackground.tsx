06b9cd1bea40671b219bb232692aa7ab17b5372c
Jason Biggs <jasonbiggs@Knight.local>
2025-02-26 16:40:49 -0600

    pulled down the techregular blog from github

--- 09/app/components/PixelateBackground.tsx
+++
@@ -1,9 +0,0 @@
-const PixelateBackground = () => {
-  return (
-    <div className="pixelateBackground fixed inset-0 z-0 opacity-10">
-      <div className="pixelate-grid absolute inset-0 bg-grid-pattern bg-grid-8 [mask-image:linear-gradient(to_bottom,white,transparent)]" />
-    </div>
-  );
-};
-
-export default PixelateBackground;

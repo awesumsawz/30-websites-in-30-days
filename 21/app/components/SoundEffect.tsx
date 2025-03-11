5b419ad6861117ea36685c177c80639f827e4943
Jason Biggs <jason@thinkbigg.dev>
2025-03-10 21:25:46 -0500

    removed the clicking noise

--- 21/app/components/SoundEffect.tsx
+++ 21/app/components/SoundEffect.tsx
@@ -3,39 +3,7 @@
 import { useEffect, useRef } from "react"
 
 const SoundEffect = () => {
-  const audioContextRef = useRef<AudioContext | null>(null)
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      audioContextRef.current = new (window.AudioContext || (window as any).webkitAudioContext)()
-    }
-
-    const playSound = () => {
-      if (audioContextRef.current) {
-        const oscillator = audioContextRef.current.createOscillator()
-        oscillator.type = "square"
-        oscillator.frequency.setValueAtTime(440, audioContextRef.current.currentTime) // 440 Hz = A4 note
-
-        const gainNode = audioContextRef.current.createGain()
-        gainNode.gain.setValueAtTime(0.1, audioContextRef.current.currentTime)
-        gainNode.gain.exponentialRampToValueAtTime(0.01, audioContextRef.current.currentTime + 0.1)
-
-        oscillator.connect(gainNode)
-        gainNode.connect(audioContextRef.current.destination)
-
-        oscillator.start()
-        oscillator.stop(audioContextRef.current.currentTime + 0.1)
-      }
-    }
-
-    document.addEventListener("click", playSound)
-
-    return () => {
-      document.removeEventListener("click", playSound)
-      audioContextRef.current?.close()
-    }
-  }, [])
-
+  // Sound effect is disabled
   return null
 }
 

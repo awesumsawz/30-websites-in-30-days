17bcbd0d1a1087a25f9b1522a75aa1b0e351a424
Jason Biggs <development@jason-biggs.com>
2025-03-21 22:19:56 -0500

    removed breadcrumbs

--- 30/resources/views/layouts/blog-post.blade.php
+++ 30/resources/views/layouts/blog-post.blade.php
@@ -2,12 +2,6 @@
 @include('components.header')
 
 <main class="blog-post-page">
-    <div class="breadcrumbs">
-        <div class="container">
-            @include('components.breadcrumbs', ['title' => $__env->yieldContent('title')])
-        </div>
-    </div>
-    
     <div class="container">
         @yield('content')
     </div>
